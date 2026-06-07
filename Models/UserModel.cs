e84b62ff2f9b59417087b5725d11f0e8ad3299ce
hashir hassan <hashir.hassan.cs@gmail.com>
2026-06-07 21:59:39 +0500

    adding user file for validation

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -1,6 +1,18 @@
-﻿namespace Application_02.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorFormApp.Models;
+
+public class UserModel
 {
-    public class UserModel
-    {
-    }
+    [Required(ErrorMessage = "First name is required.")]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "Must be 2–50 characters.")]
+    public string FirstName { get; set; } = "";
+
+    [Required(ErrorMessage = "Last name is required.")]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "Must be 2–50 characters.")]
+    public string LastName { get; set; } = "";
+
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Enter a valid email address.")]
+    public string Email { get; set; } = "";
 }
